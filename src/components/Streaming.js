f9c03df9c10ef58bcd08df04ea4687a89a66e051
dOndOnAi <adones.kjc@gmail.com>
2017-09-07 19:02:09 +0800

    Add meta

--- src/components/Streaming.js
+++ src/components/Streaming.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { FlexVideo } from 'react-foundation';
-import MetaTags from 'react-meta-tags';
+import { Helmet } from 'react-helmet';
 import logo from '../images/logo.png';
 
 export default class Streaming extends Component {
   render() {
     return (
       <div className="livestream_embed">
-        <MetaTags>
+        <Helmet>
+          <meta charSet="utf-8" />
           <title>SMNI Live Streaming</title>
-          <meta id="meta-description" name="description" content="Sonshine Media Network International (SMNI)." />
-          <meta id="og-title" property="og:title" content="SMNI" />
-          <meta id="og-type" property="og:type" content="website" />
-          <meta id="og-image" property="og:image" content={logo} />
-        </MetaTags>
+          <meta name="description" content="Sonshine Media Network International (SMNI)" />
+          <link rel="canonical" href="https://smni.com/awesome" />
+          <meta property="og:title" content="Sonshine Media Network International" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://smni.com/" />
+          <meta property="og:image" content={logo} />
+          <meta property="og:site_name" content="Sonshine Media Network International" />
+          <meta property="og:description" content="Sonshine Media Network International" />
+          <meta itemprop="image" content={logo} />
+        </Helmet>
         <FlexVideo isWidescreen>
           <iframe width="560" height="315" id="ls_embed_1504014704" title="SMNI Streaming" src="https://livestream.com/accounts/19079954/events/7685105/player?width=960&height=540&enableInfoAndActivity=false&defaultDrawer=&autoPlay=true&mute=false" allowFullScreen></iframe>
         </FlexVideo>
